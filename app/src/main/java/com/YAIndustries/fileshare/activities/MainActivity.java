c09b500af96aaf1ed0b8422605e4a79fbb96761b
Yadunandan Ahuja <yaduahuja24@gmail.com>
2022-06-04 20:17:44 +0530

    Added new API for Android 11

--- app/src/main/java/com/YAIndustries/fileshare/activities/MainActivity.java
+++ app/src/main/java/com/YAIndustries/fileshare/activities/MainActivity.java
@@ -10,7 +10,6 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.IntentFilter;
 import android.net.Uri;
@@ -23,13 +22,14 @@ import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
 import android.net.wifi.p2p.WifiP2pManager.ActionListener;
 import android.net.wifi.p2p.WifiP2pManager.ConnectionInfoListener;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
 
 import com.YAIndustries.fileshare.R;
 import com.YAIndustries.fileshare.services.MyBroadcastReceiver;
@@ -47,10 +47,15 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Observable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ThreadPoolExecutor;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
     private Button sendButton, receiveButton, testButton, searchButton;
     private WifiP2pManager manager;
     private Channel channel;
@@ -58,11 +63,18 @@ public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
     private AppCompatActivity thisActivity;
     private TextView deviceListView, filePathView;
-    private PeerListListener peerListListener;
-    private List<WifiP2pDevice> availableDeviceList = new ArrayList<>();
+    private final List<WifiP2pDevice> availableDeviceList = new ArrayList<>();
     private Uri filePath;
     private TextInputLayout portAddress, ipAddress;
     private String hostAddress = null;
+    private final ActivityResultLauncher<String> filePicker = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            (Uri result) -> {
+                filePath = result;
+                filePathView.setText(filePath.toString());
+            }
+    );
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         searchButton = findViewById(R.id.search_button);
         ipAddress = findViewById(R.id.ipAddressInput);
 
-        peerListListener = deviceList -> {
+        PeerListListener peerListListener = deviceList -> {
             Collection<WifiP2pDevice> currentPeers = deviceList.getDeviceList();
             if(!availableDeviceList.equals(currentPeers)) {
                 availableDeviceList.clear();
@@ -92,21 +104,151 @@ public class MainActivity extends AppCompatActivity {
         };
         filePath = null;
 
-        ActivityResultLauncher<String> filePicker = registerForActivityResult(
-                new ActivityResultContracts.GetContent(),
-                (Uri result) -> {
-                    filePath = result;
-                    filePathView.setText(filePath.toString());
-                }
-        );
-
         ConnectionInfoListener connectionInfoListener = (WifiP2pInfo info) -> {
             hostAddress = info.groupOwnerAddress.getHostAddress();
             showToast(this, "Host Address is : "+ hostAddress);
             ipAddress.getEditText().setText(hostAddress);
         };
 
-        searchButton.setOnClickListener((listener) -> {
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+        receiver = new MyBroadcastReceiver(manager, channel, this, peerListListener, connectionInfoListener);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        searchButton.setOnClickListener(this);
+        sendButton.setOnClickListener(this);
+        receiveButton.setOnClickListener(this);
+        deviceListView.setOnClickListener(this);
+        testButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, intentFilter);
+    }
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    private void createServer() {
+        try {
+            /**
+             * Create a server socket and wait for client connections. This
+             * call blocks until a connection is accepted from a client
+             */
+            ServerSocket serverSocket = new ServerSocket(0);
+            Log.d("Server Creation ", "Port "+ serverSocket.getLocalPort());
+            runOnUiThread(()-> {
+                portAddress.getEditText().setText(Integer.toString(serverSocket.getLocalPort()));
+            });
+            Log.d("Server Creation", "Address" + serverSocket.getInetAddress().toString());
+            Socket client = serverSocket.accept();
+            Log.d("Server Creation","Address "+ client.getInetAddress().toString());
+            runOnUiThread(()-> {
+                ipAddress.getEditText().setText(client.getInetAddress().toString());
+            });
+            /**
+             * If this code is reached, a client has connected and transferred data
+             * Save the input stream from the client as a JPEG file
+             */
+            File file = new File(Environment.getExternalStorageDirectory() + "/Download/"
+                    +"FileShare/" + System.currentTimeMillis()
+                    + ".jpg");
+            File directory = file.getParentFile();
+            if (directory == null || !directory.mkdirs()){
+                Log.d("Directory Check", "Directory not created");
+                runOnUiThread(() -> showToast(this, "File Cannot be Created"));
+            }
+
+            boolean fileCreated = file.createNewFile();
+            if(fileCreated)
+                runOnUiThread(() -> showToast(this, "File creation Successful"));
+            Log.d("Directory Check", "File Created : "+ fileCreated);
+            InputStream inputstream = client.getInputStream();
+            Utils.copyFile(this, inputstream, new FileOutputStream(file));
+            client.close();
+            serverSocket.close();
+        } catch (Exception e) {
+            Log.d("Service Executor Exception ", e.getMessage());
+            runOnUiThread(() -> showToast(this, e.getMessage()));
+        }
+    }
+
+    private String sendDataToServer(String hostAddress, int port) {
+        Socket socket = new Socket();
+        String ret = null;
+        try {
+            socket.bind(null);
+            socket.connect(new InetSocketAddress(hostAddress, port), 500);
+            runOnUiThread(() -> showToast(this, "Connection Successful"));
+            OutputStream outputStream = socket.getOutputStream();
+            var inputStream = getContentResolver().openInputStream(filePath);
+            Utils.copyFile(this, inputStream, outputStream);
+            runOnUiThread(()-> showToast(this,"Data Sent Successfully"));
+        } catch (Exception e) {
+            Log.d("Send Data to Server", e.getMessage());
+            ret = e.getMessage();
+        }
+        //Cleanup
+        finally {
+            if (socket.isConnected()) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    Log.d("Send Data to Server", e.getMessage());
+                    ret = e.getMessage();
+                }
+            }
+        }
+        if(ret == null)
+            return "File Transfer Successful";
+        return  ret;
+    }
+
+    private void cleanUp() {
+        manager.removeGroup(channel, new ActionListener() {
+            @Override
+            public void onSuccess() {
+                showToast(thisActivity, "Cleanup Successful");
+                Log.d("Cleanup", "Successful");
+            }
+
+            @Override
+            public void onFailure(int i) {
+                showToast(thisActivity, "Cleanup Error");
+                Log.d("Cleanup", "Unsuccessful");
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == sendButton) {
+            if(filePath == null) {
+                filePicker.launch("*/*");
+                return;
+            }
+            String hostAddress = ipAddress.getEditText().getText().toString();
+            int port = Integer.parseInt(portAddress.getEditText().getText().toString());
+            Executors.newSingleThreadExecutor().execute(() -> {
+                try {
+                    sendDataToServer(hostAddress, port);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        if(view == searchButton) {
             if(!checkLocationPermission(this))
                 return;
             manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
@@ -121,25 +263,9 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("Wifip2p Action Listener", "Failure Reason Code : "+ reasonCode);
                 }
             });
-        });
+        }
 
-        sendButton.setOnClickListener((listener) -> {
-            if(filePath == null) {
-                filePicker.launch("*/*");
-                return;
-            }
-            String hostAddress = ipAddress.getEditText().getText().toString();
-            int port = Integer.parseInt(portAddress.getEditText().getText().toString());
-            Executors.newSingleThreadExecutor().execute(() -> {
-                try {
-                    sendDataToServer(hostAddress, port);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-        });
-
-        receiveButton.setOnClickListener((listener) -> {
+        if(view == receiveButton) {
             manager.createGroup(channel, new ActionListener() {
                 @Override
                 public void onSuccess() {
@@ -154,14 +280,10 @@ public class MainActivity extends AppCompatActivity {
             showToast(this, "Receive Button Clicked");
             if(!checkReadStoragePermission(this) || !checkWriteStoragePermission(this))
                 return;
-            ExecutorService service = Executors.newSingleThreadExecutor();
-            service.execute(() -> {
-                String res = createServer();
-                Log.d("Server Result", res+":Error");
-            });
-        });
+            CompletableFuture.runAsync(this::createServer).thenRunAsync(this::cleanUp);
+        }
 
-        deviceListView.setOnClickListener((listener) -> {
+        if(view == deviceListView) {
             if(availableDeviceList.isEmpty())
                 return;
 
@@ -184,154 +306,28 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("Connection Stats : ", "Connection Success");
                 }
             });
-        });
+        }
 
-        testButton.setOnClickListener((listener) -> {
+        if(view == testButton) {
             if(!checkWriteStoragePermission(this) || !checkReadStoragePermission(this)){
                 showToast(this, "Storage Permission not Granted", Toast.LENGTH_SHORT);
                 return;
             }
-            if(filePath == null)
+            if(filePath == null) {
                 filePicker.launch("*/*");
+                return;
+            }
+
             try {
-                File file = new File(Environment.getExternalStorageDirectory() + "/"
-                        +"FileShare/" + System.currentTimeMillis()
-                        + ".jpg");
-                ContentResolver cr = getContentResolver();
-                InputStream fis = cr.openInputStream(filePath);
-                File directory = file.getParentFile();
-                if (directory == null){
-                    Log.d("Directory Check", "Directory not created");
-                    return;
-                }
-
-                if(!directory.isDirectory()){
-                    directory.mkdir();
-                }
-
-
-                boolean fileCreated = file.createNewFile();
-                if(fileCreated)
-                    showToast(this, "File creation Successful");
-                FileOutputStream fos = new FileOutputStream(file);
-                Log.d("Directory Check", "File Created : "+ fileCreated);
-                Executors.newSingleThreadExecutor().execute(() -> Utils.copyFile(this, fis, fos));
-
-            }catch (Exception e) {
+                var resolver = Environment.getExternalStorageDirectory().toString()+"/Download/Fileshare/"+ System.currentTimeMillis()+".jpg";
+                File f = new File(resolver);
+                f.getParentFile().mkdir();
+                var inputStream = getContentResolver().openInputStream(filePath);
+                var fos = new FileOutputStream(f);
+                Utils.copyFile(this, inputStream, fos);
+            } catch (Exception e) {
                 e.printStackTrace();
-                Log.d("File Handler", e.getMessage());
-            }
-        });
-
-
-
-        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = manager.initialize(this, getMainLooper(), null);
-        receiver = new MyBroadcastReceiver(manager, channel, this, peerListListener, connectionInfoListener);
-
-        intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(receiver, intentFilter);
-    }
-    /* unregister the broadcast receiver */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
-    }
-
-    private String createServer() {
-        try {
-            /**
-             * Create a server socket and wait for client connections. This
-             * call blocks until a connection is accepted from a client
-             */
-            ServerSocket serverSocket = new ServerSocket(0);
-            Log.d("Server Creation ", "Port "+ serverSocket.getLocalPort());
-            Log.d("Server Creation", "Address" + serverSocket.getInetAddress().toString());
-            Socket client = serverSocket.accept();
-            Log.d("Server Creation","Address "+ client.getInetAddress().toString());
-            runOnUiThread(()-> {
-                showToast(this, "Server Created at Port " + serverSocket.getLocalPort());
-                portAddress.getEditText().setText(client.getInetAddress().toString());
-            });
-            /**
-             * If this code is reached, a client has connected and transferred data
-             * Save the input stream from the client as a JPEG file
-             */
-            File file = new File(Environment.getExternalStorageDirectory() + "/"
-                    +"FileShare/" + System.currentTimeMillis()
-                    + ".jpg");
-            File directory = file.getParentFile();
-            if (directory == null || !directory.mkdirs()){
-                Log.d("Directory Check", "Directory not created");
-                runOnUiThread(() -> showToast(this, "File Cannot be Created"));
-            }
-
-            boolean fileCreated = file.createNewFile();
-            if(fileCreated)
-                runOnUiThread(() -> showToast(this, "File creation Successful"));
-            Log.d("Directory Check", "File Created : "+ fileCreated);
-            InputStream inputstream = client.getInputStream();
-            Utils.copyFile(this, inputstream, new FileOutputStream(file));
-            client.close();
-            serverSocket.close();
-            return file.getAbsolutePath();
-        } catch (IOException e) {
-            Log.d("Service Executor", e.getMessage());
-            return null;
-        } catch (Exception e) {
-            Log.d("Service Executor Exception ", e.getMessage());
-            runOnUiThread(() -> showToast(this, e.getMessage()));
-            return null;
-        }
-    }
-
-    private String sendDataToServer(String hostAddress, int port) throws Exception {
-        Socket socket = new Socket();
-        String ret = null;
-        try {
-            socket.bind(null);
-            socket.connect(new InetSocketAddress(hostAddress, port), 500);
-
-            runOnUiThread(() -> showToast(this, "Connection Successful"));
-
-            /**
-             * Create a byte stream from a JPEG file and pipe it to the output stream
-             * of the socket. This data is retrieved by the server device.
-             */
-            OutputStream outputStream = socket.getOutputStream();
-            ContentResolver cr = getContentResolver();
-            InputStream inputStream = cr.openInputStream(filePath);
-            Utils.copyFile(this, inputStream, outputStream);
-            inputStream.close();
-            outputStream.close();
-            runOnUiThread(()-> showToast(this,"Data Sent Successfully"));
-        } catch (Exception e) {
-            Log.d("Send Data to Server", e.getMessage());
-            ret = e.getMessage();
-        }
-        //Cleanup
-        finally {
-            if (socket.isConnected()) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    Log.d("Send Data to Server", e.getMessage());
-                    ret = e.getMessage();
-                }
             }
         }
-        if(ret == null)
-            return "File Transfer Successful";
-        return  ret;
     }
 }
--- app/src/main/java/com/YAIndustries/fileshare/utilities/Utils.java
+++ app/src/main/java/com/YAIndustries/fileshare/utilities/Utils.java
@@ -3,13 +3,13 @@ package com.YAIndustries.fileshare.utilities;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -25,11 +25,7 @@ public class Utils {
     public static boolean checkLocationPermission(AppCompatActivity activity) {
         if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 12);
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 showToast(activity, "Permission Not Granted");
                 Log.d("Permission Request", "Not Granted");
@@ -41,13 +37,10 @@ public class Utils {
     }
 
     public static boolean checkWriteStoragePermission(AppCompatActivity activity) {
+        if(checkVersionAboveQ())
+            return true;
         if(ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 12);
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 showToast(activity, "Permission Not Granted");
                 Log.d("Permission Request", "Not Granted");
@@ -61,11 +54,6 @@ public class Utils {
     public static boolean checkReadStoragePermission(AppCompatActivity activity) {
         if(ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 12);
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 showToast(activity, "Permission Not Granted");
                 Log.d("Permission Request", "Not Granted");
@@ -77,8 +65,9 @@ public class Utils {
     }
 
     public static void copyFile(AppCompatActivity activity, InputStream inputStream, OutputStream outputStream) {
-        int count = 0, c, len = 1<<18;
-        byte buf[] = new byte[len];
+        int c, len = 1<<18;
+        long count = 0;
+        byte[] buf = new byte[len];
         try{
             while ((len = inputStream.read(buf)) != -1){
                 outputStream.write(buf, 0, len);
@@ -91,7 +80,11 @@ public class Utils {
             activity.runOnUiThread(()-> showToast(activity, "Error in Copy"));
         }
         Log.d("Copy File", "Bytes Written : "+ count);
-        final int countCopy = count;
+        final long countCopy = count;
         activity.runOnUiThread(() -> showToast(activity, "Bytes Written : "+ countCopy));
+    }
+
+    public static boolean checkVersionAboveQ() {
+        return Build.VERSION_CODES.Q < Build.VERSION.SDK_INT;
     }
 }
